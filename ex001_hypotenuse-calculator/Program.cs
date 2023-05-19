7d75d52377d5ac6eea9375145dd610373f05c109
diogobnfr <diogobonofre@gmail.com>
2023-05-19 01:20:37 -0300

    Added hypotenuse-calculator

---
+++ ex001_hypotenuse-calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("A leg: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("B leg: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+Console.WriteLine("Hypotenuse: " + c);

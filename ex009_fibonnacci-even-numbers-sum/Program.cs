3d8ceb64669429df53276ecdfbf2ac3947843728
diogobnfr <diogobonofre@gmail.com>
2023-06-29 19:43:00 -0300

    Exercise Fibonnacci Sequence even number sum

---
+++ ex009_fibonnacci-even-numbers-sum/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex009_fibonnacci_even_numbers_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int previous = 0;
+            int current = 1;
+            int next = 0;
+            int sum = 0;
+            while (next <= 4000000)
+            {
+                next = previous + current;
+                if (next % 2 == 0) sum += next;
+                previous = current;
+                current = next;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

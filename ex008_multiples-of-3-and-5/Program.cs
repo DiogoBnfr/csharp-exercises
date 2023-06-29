6341e3b65072fd04d76cb9c9d27cfa3204b3e9bf
diogobnfr <diogobonofre@gmail.com>
2023-06-29 18:47:42 -0300

    Multiples of 3 and 5 exercise

---
+++ ex008_multiples-of-3-and-5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ex008_multiples_of_3_and_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert range: ");
+            int range = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0;  i < range; i++)
+            {
+                if (i % 3 == 0  || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"The sum of the multiples of 3 and 5 below {range} is: {sum}");
+        }
+    }
+}
